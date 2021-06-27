4ad0717b7437bd5a4f64b6897f5f0af240e36c35
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-06-27 16:40:52 +0300

    PHP code

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$txt = "Welcome to my Website";
+echo "Hello this is Wangari";
+//string concatenation
+  
+echo "Code" . "cademy";
+
+  echo "\nMy name is:" . " " . "Susan Wangari";
+  echo "\n" . "tur" . "duck" . "en";
+?>
